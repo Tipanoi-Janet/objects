93c28d76c6801eb60f2e6b6032965935f2fe997a
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-05-03 00:20:25 +0300

    changes

--- inheritance.js
+++ inheritance.js
@@ -33,7 +33,7 @@ console.log({car});  this.isAvailable === this.isAvailable
 //The Rental class or function constructor should also have a method called
 //calculateRentalDuration that returns the rental duration in days.
     
-class Rental{
+ class Rental{
     constructor(object,renterName,rentalStartDate,rentalEndDate){
         this.object=object
         this.renterName=renterName
@@ -44,8 +44,8 @@ class Rental{
         this.rentalEndDate=!this.rentalEndDate
     }
 }
-let rental=new Rental("tesla","janet,30),
- console.log(Rental)
+let rental=new classRental("Tesla","Janet",30)
+  var console:classRental
 
 
 
@@ -54,60 +54,73 @@ let rental=new Rental("tesla","janet,30),
 //inventory. Then, create an instance of the Rental class or function constructor for
 //a rental involving the car you created. Finally, calculate the rental duration using
 //the calculateRentalDuration method
+  
+// create car instance
+const car = new Car('Toyota', 'Camry', 2020, true)
+// create rental instance
+const rentalStartDate = new Date('2023-04-01')
+const rentalEndDate = new Date('2023-04-10')
+const rentalDuration = new Rental(car, 'Jane Doe', rentalStartDate, rentalEndDate)
+const rentalduration = rental.carRentalDuration()
+console.log('rental Duration in days:', rentalDuration)
+ 
 
-class car{
-
-    constructor(make,model,year,isavailable){
-        this.make=make;
-        this.model=model;
-        this.year=year;
-        this.isavailability=isavailable;
-        this.toggleAvailability=toggleAvailability;
-//     constructor(model,make, colour){
-//         this.model=model;
-//         this.make=make;
-//         this.colour=colour
-//     }
-// }
+    
+   class Question {
+    constructor(text, options, correctAnswer){
+        this.text = text,
+        this.options = options,
+        this.correctAnswer = correctAnswer
     }
-
+    checkAnswer(usersAnswer){
+        return usersAnswer === this.correctAnswer
+    }
 }
-//you are building a simple quiz app that contains multiple-choice questions. Your task is
-//to create two JavaScript classes: Question and Quiz. The Question class will represent
-//individual questions, and the Quiz class will manage a collection of questions and the
-//user's progress.
-class question{
-    
-    
+class Quiz {
+    constructor(){
+        this.questions = [],
+        this.currentQuestionIndex = 0,
+        this.score = 0
+    }
+    addQuestion(question){
+        this.questions.push(question)
+    }
+    nextQuestion(){
+        this.currentQuestionIndex++
+    }
+    submitAnswer(usersAnswer){
+        const currentQuestion = this.questions[this.currentQuestionIndex]
+        if (currentQuestion.checkAnswer(usersAnswer)){
+            this.score++
+        }
+        this.nextQuestion()
+    }
 }
+const quiz = new Quiz()
+const question1 = new Question(
+    "What is the capital of France?",
+    ["Paris", "London", "Berlin", "Rome"],
+    "Paris"
+  );
+  const question2 = new Question(
+    "Which planet is known as the Red Planet?",
+    ["Venus", "Venus", "Jupiter", "Saturn"],
+    "jupiter"
+  );
+quiz.addQuestion(question1)
+quiz.addQuestion(question2)
+quiz.submitAnswer('Paris')
+quiz.submitAnswer('Venus')
+console.log('Score:', quiz.score)
+ 
 
 
 
 
-1//. Create a Question class with the following properties:
- //text(string): The text of the question.
- //options(array): An array containing the multiple-choice options.
-//correctAnswer(string): The correct answer to the question.
-//The Question class should also have a method called checkAnswer that takes a
-//user's answer as a parameter and returns true if the answer is correct and false
-//otherwise.
-class question
 
 
 
 
 
 
-// Create a Quiz class with the following properties:
-//questions(array):An array of Question objects.
-// currentQuestionIndex(number): The index of the current question in the
-//questions array.
-// score(number): The user’s current score.
-//The Quiz class should have the following methods:
-//addQuestion: Takes a Question object as a parameter and adds it to the
-//questions array.
-// nextQuestion: Advances to the next question in the quiz by incrementing the
-//currentQuestionIndex.
-// submitAnswer: Takes a user's answer as a parameter, checks if the answer is
-//correct using the checkAnswer method of the Question class, and updates the
-//score if the answer is correct.
+
